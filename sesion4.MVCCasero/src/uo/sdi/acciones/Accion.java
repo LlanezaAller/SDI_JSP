e83f46d483661d74fd58a573697c29861081d4e1
LlanezaAller <uo206367@uniovi.es>
2016-02-22 21:30:19 +0100

    .

--- sesion4.MVCCasero/src/uo/sdi/acciones/Accion.java
+++ sesion4.MVCCasero/src/uo/sdi/acciones/Accion.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Accion {
-	
 	public String execute(HttpServletRequest request, 
 			HttpServletResponse response);
 
