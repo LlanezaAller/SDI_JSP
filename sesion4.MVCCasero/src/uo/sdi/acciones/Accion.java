9c12b09b347f17f720f000091e06ff917ce42b81
LlanezaAller <llan090@gmail.com>
2016-02-23 11:13:39 +0100

    Prueba Portátil

--- sesion4.MVCCasero/src/uo/sdi/acciones/Accion.java
+++ sesion4.MVCCasero/src/uo/sdi/acciones/Accion.java
@@ -4,8 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Accion {
-	
+
 	public String execute(HttpServletRequest request, 
 			HttpServletResponse response);
-
 }
