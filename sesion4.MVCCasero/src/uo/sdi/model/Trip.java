07fd656042cd52782f3faff588c742c851dc3c1c
LlanezaAller <llan090@gmail.com>
2016-02-25 21:13:11 +0100

    mod

--- sesion4.MVCCasero/src/uo/sdi/model/Trip.java
+++ sesion4.MVCCasero/src/uo/sdi/model/Trip.java
@@ -14,7 +14,6 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
