dfb0ea803806667cc3b5fcf9f77e31e854414c3b
LlanezaAller <llan090@gmail.com>
2016-02-26 20:43:38 +0100

    findRatingsByUserAboutId

--- sesion4.MVCCasero/src/uo/sdi/persistence/impl/RatingFinderImpl.java
+++ sesion4.MVCCasero/src/uo/sdi/persistence/impl/RatingFinderImpl.java
@@ -12,12 +12,22 @@ public class RatingFinderImpl implements RatingFinder {
 	public Rating findByAboutFrom(Long aboutUserId, Long aboutTripId,
 			Long fromUserId, Long fromTripId) {
 		List<Rating> ratings = Jpa.getManager()
-				.createNamedQuery("User.findUserByLogin",
+				.createNamedQuery("Rating.findByAboutFrom",
 						Rating.class)
 				.setParameter(1, aboutUserId)
 				.setParameter(2, aboutTripId)
 				.getResultList();
 		return (ratings.size() > 0) ? ratings.get(0) : null;
 	}
+
+	@Override
+	public List<Rating> findRatingsByUserAboutId(Long id) {
+		return Jpa.getManager()
+				.createNamedQuery("Rating.findRatingsByUserAboutId",
+						Rating.class)
+				.setParameter(1, id)
+				.getResultList();
+	}
+	
 
 }
