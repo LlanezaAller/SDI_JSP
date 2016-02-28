74f7f92cb5135a7ce6a6ddea84e9cc8c7c093316
LlanezaAller <llan090@gmail.com>
2016-02-29 00:41:34 +0100

    ready

--- sesion4.MVCCasero/src/uo/sdi/persistence/SeatFinder.java
+++ sesion4.MVCCasero/src/uo/sdi/persistence/SeatFinder.java
@@ -1,10 +1,13 @@
 package uo.sdi.persistence;
 
+import java.util.List;
+
 import uo.sdi.model.Seat;
 
 public interface SeatFinder {
 
 	Seat findByUserAndTrip(Long userId, Long tripId);
 	void newSeat(Seat seat);
+	List<Seat> findByUserAndTrip(Long userId);
 
 }
--- sesion4.MVCCasero/src/uo/sdi/persistence/impl/SeatFinderImpl.java
+++ sesion4.MVCCasero/src/uo/sdi/persistence/impl/SeatFinderImpl.java
@@ -1,8 +1,10 @@
 package uo.sdi.persistence.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import uo.sdi.model.Seat;
+import uo.sdi.model.Trip;
 import uo.sdi.persistence.SeatFinder;
 import uo.sdi.persistence.util.Jpa;
 
@@ -16,6 +18,15 @@ public class SeatFinderImpl implements SeatFinder {
 				.setParameter(2, tripId)
 				.getResultList();
 		return (seats.size() > 0) ? seats.get(0) : null;
+	}
+	@Override
+	public List<Seat> findByUserAndTrip(Long userId) {
+		List<Seat> seats = Jpa.getManager()
+				.createNamedQuery("Seat.findByUser",
+						Seat.class)
+						.setParameter(1, userId)
+						.getResultList();
+		return (seats != null) ? seats : new ArrayList<Seat>();
 	}
 
 	@Override
